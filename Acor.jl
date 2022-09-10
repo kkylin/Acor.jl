f6ceb357af94a81d9fafe353d73d19c8bdc44cef
Kevin K Lin <klin@math.arizona.edu>
2022-09-10 11:59:58 -0700

    allow julia Int input

--- Acor.jl
+++ Acor.jl
@@ -9,30 +9,30 @@ cd(()->run(`make`), MYDIR)
 
 ACORPATH = MYDIR*"/acor"
 
-function acor(X::Vector{Cdouble}, maxlag::Cint)
+function acor(X::Vector{Cdouble}, maxlag::Int)
 
     mean  = [0.0]
     sigma = [0.0]
     tau   = [0.0]
 
     status = ccall(("acor", ACORPATH),
-          Cint, # int
-          (
-              Ptr{Cdouble}, # double *mean
-              Ptr{Cdouble}, # double *sigma
-              Ptr{Cdouble}, # double *tau
-              Ptr{Cdouble}, # double X[]
-              Cint,         # int L
-              Cint,         # int maxlag
-          ),
-          pointer(mean),
-          pointer(sigma),
-          pointer(tau),
-          pointer(X),
-          length(X),
-          maxlag)
+                   Cint, # int
+                   (
+                       Ptr{Cdouble}, # double *mean
+                       Ptr{Cdouble}, # double *sigma
+                       Ptr{Cdouble}, # double *tau
+                       Ptr{Cdouble}, # double X[]
+                       Cint,         # int L
+                       Cint,         # int maxlag
+                   ),
+                   pointer(mean),
+                   pointer(sigma),
+                   pointer(tau),
+                   pointer(X),
+                   length(X),
+                   Cint(maxlag))
 
     (mean=mean[1],sigma=sigma[1],tau=tau[1],status=status)
 end
-      
+
 end#module
